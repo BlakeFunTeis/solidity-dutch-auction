36a7601dd04033d542697d2591071b48933d9d1d
BlakeFunTeis <tk0wo3@gmail.com>
2023-02-14 09:12:03 +0800

    Add constructor and define vars

--- DutchAuction.sol
+++ DutchAuction.sol
@@ -8,4 +8,157 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract DutchAction is Ownable, ERC721A {
 
+    // Import the SafeMath library
+    using SafeMath for uint;
+
+    struct tokenBetchPrice {
+        uint mintPrice;
+        uint total;
+    }
+
+    mapping (address => tokenBetchPrice) private allMintData;
+
+    // Define the online status
+    bool private online = true;
+
+    // Define the revealed
+    bool private revealed = true;
+
+    // Define the baseURI
+    string private baseURI;
+
+    // Define the start time
+    uint private startTimestamp = 1676268000;
+
+    // Define the end time
+    uint private endTimestamp = 1676390400;
+
+    // Define the default price
+    uint private defaultPricePerToken = 2 ether;
+
+    // Define the decay rate
+    uint private priceDecayRate = 0.05 ether;
+
+    // Define the time interval for decay
+    uint private decayInterval = 900;
+
+    // Define the minimum price
+    uint private minimumPricePerToken = 0.35 ether;
+
+    // Define the total supply
+    uint private supply = 4000;
+
+    // Define the mint max NFTs
+    uint private mintLimit = 2;
+
+    constructor(string memory _name, string memory _symbol) ERC721A(_name, _symbol) {}
+
+    /**
+    * @dev function to set the base URI for NFTs
+    * @param _uri base URI string
+    */
+    function setBaseURI(string memory _uri) external  onlyOwner {
+        baseURI = _uri;
+    }
+
+    /**
+    * @dev modifier to check if the caller is a normal user
+    */
+    modifier isNormalUser() {
+        require(tx.origin == msg.sender, "The caller is another contract");
+        _;
+    }
+
+    /**
+    * @dev function to get the token URI for a specific token
+    * @param _tokenId the token id
+    * @return string memory token URI
+    */
+    function tokenURI(uint _tokenId) public view override returns(string memory) {
+        require(_exists(_tokenId), "URI query for nonexistent token");
+        if (!revealed) return baseURI;
+        return string(abi.encodePacked(baseURI, Strings.toString(_tokenId), ".json"));
+    }
+
+    /**
+    * @dev modifier to check if the conditions for minting NFTs are met
+    * @param _total total number of NFTs to mint
+    */
+    modifier isCheckMint(uint _total) {
+        require((allMintData[msg.sender].total + _total) <= mintLimit, "Can only mint max number NFTs");
+        require(block.timestamp <= endTimestamp, "DutchAuction is finished!");
+        require(block.timestamp >= startTimestamp, "DutchAuction has not started!");
+        require(online == true, "DutchAuction is Offline");
+        require(_total + totalSupply() <= supply, "Max Supply for DutchAuction reached!");
+        _;
+    }
+    
+    /**
+    * @dev function to set the online status of the contract
+    * @param _online true if the contract is online, false otherwise
+    */
+    function setOnline(bool _online) public onlyOwner {
+        online = _online;
+    }
+
+    /**
+    * @dev function to set the revealed status of the contract
+    * @param _revealed true if the contract is revealed, false otherwise
+    */
+    function setRevealed(bool _revealed) public onlyOwner {
+        revealed = _revealed;
+    }
+
+    /**
+    * @dev function to set the start timestamp of the contract
+    * @param _startTimestamp start timestamp
+    */
+    function setStartTimestamp(uint _startTimestamp) public onlyOwner {
+        startTimestamp = _startTimestamp;
+    }
+
+    /**
+    * @dev function to set the end timestamp of the contract
+    * @param _endTimestamp end timestamp
+    */
+    function setEndTimestamp(uint _endTimestamp) public onlyOwner {
+        endTimestamp = _endTimestamp;
+    }
+
+    /**
+    * @dev function to set the price decay rate of the contract
+    * @param _priceDecayRate price decay rate
+    */
+    function setPriceDecayRate(uint _priceDecayRate) public onlyOwner {
+        priceDecayRate = _priceDecayRate;
+    }
+
+    /**
+    * @dev function to set the decay interval of the contract
+    * @param _decayInterval decay interval
+    */
+    function setDecayInterval(uint _decayInterval) public onlyOwner {
+        decayInterval = _decayInterval;
+    }
+    /**
+    * @dev function to set the minimum price per token of the contract
+    * @param _minimumPricePerToken minimum price per token
+    */
+    function setMinimumPricePerToken(uint _minimumPricePerToken) public onlyOwner {
+        minimumPricePerToken = _minimumPricePerToken;
+    }
+    /**
+    * @dev function to set the total supply of the contract
+    * @param _supply total supply
+    */
+    function setSupply(uint _supply) public onlyOwner {
+        supply = _supply;
+    }
+    /**
+    * @dev function to set the mint limit of the contract
+    * @param _mintLimit mint limit
+    */
+    function setMintLimit(uint _mintLimit) public onlyOwner {
+        mintLimit = _mintLimit;
+    }
 }
